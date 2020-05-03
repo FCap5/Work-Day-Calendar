665cbf3bc3004ab402807552a35e2f3c7ad871d6
Ffej Caplan <ffej.caplan@gmail.com>
2020-05-03 13:44:48 -0400

    fixed some of the html to make cleaner. Updated JS and CSS to match. started readME

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,46 +1,57 @@
-$(document).ready(function () { });
+$(document).ready(function () {});
 
 //Set today's date
-var todayDate = moment().format("dddd" + ", " + "MMMM" + " " + "Do" + ", " + "YYYY");
+var todayDate = moment().format(
+  "dddd" + ", " + "MMMM" + " " + "Do" + ", " + "YYYY"
+);
 $("#timeStamp").text(todayDate);
 var currentHour = moment().format("HH");
 //determining the number of children of the scheduler div (number of hours in the day)
 // https://stackoverflow.com/questions/250688/count-immediate-child-div-elements-using-jquery
 var scheduleHours = $("#scheduler > div");
 
-//console.log(currentHour);
-
 //loop through the hours
 for (i = 0; i < scheduleHours.length; i++) {
-    var hour = $(scheduleHours[i]);
-    var timeSlot = $(hour).attr("id");
-    //see if the hour has passed
-    if (parseInt(timeSlot) < currentHour) {
-        $("#" + timeSlot).children().children("#calendarEvent").css({ "background-color": "gray" });
-        //see if it's the current hour
-    } else if (timeSlot === currentHour) {
-        $("#" + timeSlot).children().children("#calendarEvent").css({ "background-color": "white" });
-    }
-};
+  var hour = $(scheduleHours[i]);
+  var timeSlot = $(hour).attr("id");
+  //see if the hour has passed
+  //used parseInt to get the integer value of the scheduleHours id
+  if (parseInt(timeSlot) < currentHour) {
+    $("#" + timeSlot)
+      .children("#calendarEvent")
+      .css({ "background-color": "gray" });
+    $("#" + timeSlot)
+      .children("#calendarEvent")
+      .css({ "background-color": "gray" });
+    //see if it's the current hour
+  } else if (timeSlot === currentHour) {
+    $("#" + timeSlot)
+      .children("#calendarEvent")
+      .css({ "background-color": "white" });
+    $("#" + timeSlot)
+      .children("#calendarEvent")
+      .css({ "background-color": "white" });
+  }
+}
 
 // set input field value to local storage
 // set key to time slot ID
 $(".btn").click(function (event) {
-    event.preventDefault();
-    var inputID = $(this).parent().attr("id");
-    var scheduledItem = $(this).parent().children().children("#calendarEvent").val();
-    localStorage.setItem(inputID, scheduledItem);
+  event.preventDefault();
+  var inputID = $(this).parent().attr("id");
+  var scheduledItem = $(this).parent().children("#calendarEvent").val();
+  localStorage.setItem(inputID, scheduledItem);
 });
 
-//+ ", " + JSON.stringify(
-
+//add items from local storage to the appropriate time slots
+//since the key is set to the ID for each text input field, which in turn
+//is set to the military timestamp for that hour, the loop needs to start at 9 and go to 17
+//hence scheduleHours.length+9
 for (x = 9; x < scheduleHours.length + 9; x++) {
-    if (x === parseInt(($("#" + x).attr("id")))) {
-        $("#" + x).children().children("#calendarEvent").text(localStorage.getItem(x));
-    }
-
+  //x needs to equal the value of each time slots id
+  if (x === parseInt($("#" + x).attr("id"))) {
+    $("#" + x)
+      .children("#calendarEvent")
+      .text(localStorage.getItem(x));
+  }
 }
-
-//}
-
-
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 05_HW
+
+## What even is a "workday"?
+
+Workdays seem so fluid these days. This assignment almost seemed like taunting! But, it had some really interesting
